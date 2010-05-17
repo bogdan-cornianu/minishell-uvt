ad3ae3a0894e9a15392c44d57acc099832aa4be0
morion4000 <morion4000@8ab3d67a-2146-11df-8e4f-83d9437f54e1>
2010-05-17 01:26:55 +0000

    ...

---
+++ src/loader.c
@@ -0,0 +1,16 @@
+// Require libraries
+#include <stdio.h>
+
+// Require API file
+#include "API.c"
+
+// Require file to include commands
+#include "include_comands.h"
+
+// Require API file
+
+
+
+int main(int argc, char** argv) {
+	return 1;
+}
