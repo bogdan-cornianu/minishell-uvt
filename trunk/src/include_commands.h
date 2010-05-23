e49e19c342bad77aae319bca049404c5e04c1006
morion4000 <morion4000@8ab3d67a-2146-11df-8e4f-83d9437f54e1>
2010-05-23 22:30:32 +0000

    integration test2

--- trunk/src/include_commands.h
+++ trunk/src/include_commands.h
@@ -4,6 +4,6 @@
 #include "commands/rm.c"
 #include "commands/cd.c"
 #include "commands/dir.c"
-#include "commands/ls.c"
+#include "commands/ls_integrate.c"
 #include "commands/printenv.c"
 #include "commands/uname_integrate.c"
