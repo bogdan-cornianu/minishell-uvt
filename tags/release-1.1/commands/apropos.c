82d26fbbb9ca58a9f5b4d3a132c25dbe5f70086b
morion4000 <morion4000@8ab3d67a-2146-11df-8e4f-83d9437f54e1>
2010-05-23 23:21:22 +0000

    release 1.1

---
+++ tags/release-1.1/commands/apropos.c
@@ -0,0 +1,109 @@
+/**
+ * @file apropos.c
+ * @author Patricia Minica, anul 2 Informatica - Engleza
+ * @version 1.0
+ *
+ * display the man pages for argument
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+/**
+ * function executed to display the man pages for argument
+ * 
+ * @param argcounter  - calling arguments counter
+ * @param **argvector - calling arguments vector
+ * 	
+ * @return 0
+ */
+int apropos_default (int argcounter, char **argvector);
+
+/**
+ * command main function
+ * 
+ * @param argc   - calling arguments counter
+ * @param **argv - calling arguments vector
+ * 	
+ * @return 0 on success or a positive int on failure
+ */
+int main (int argc, char **argv) {
+
+    optind = 0;
+    int o;
+
+    while ((o = getopt (argc, argv, "hu")) != -1) {
+        switch (o) {
+            case 'h':
+                printf ("-h, display this help and exit\n");
+                printf ("-u, shows Author and Version info and exit\n");
+                return 0;
+            case 'u':
+                printf ("Author:\tPatricia Minica\n");
+                printf ("Version:\t1.0\n");
+                return 0;
+            default:
+                return 2;
+        }
+    }
+
+    /**
+	 * @see run command if argc contais 1 more argument
+	 * @see whatis_default (argc, argv);
+	 */
+    if (argc == 2) {
+        return apropos_default (argc, argv);
+    }
+
+    printf ("\n");
+
+    return 0;
+}
+
+int apropos_default (int argcounter, char **argvector)
+{
+	int flagPrintNextLine = 0;
+	int bytes_read;
+  	size_t nbytes = 100;
+	char *manline;
+	
+	FILE *manCmdFile;
+	
+	manCmdFile = fopen (strcat (argvector[1], ".txt"), "r");
+	
+	if (manCmdFile == NULL)
+	{
+		fprintf (stderr, "Man file for command %s cannot be opened\n", argvector[1]);
+		return 1;
+	}
+	
+	manline = (char *) malloc (nbytes + 1);
+	
+  	bytes_read = getline (&manline, &nbytes, manCmdFile);
+
+	while (bytes_read != -1)
+	{
+		if (strstr (manline, "NAME") != NULL)
+		{
+			flagPrintNextLine = 1;
+		}
+		else if (strstr (manline, "SYNOPSIS") != NULL)
+		{
+			flagPrintNextLine = 0;
+		}
+		else
+		{
+			if (flagPrintNextLine)
+			{
+				printf ("%s", manline);
+			}
+		}
+		bytes_read = getline (&manline, &nbytes, manCmdFile);
+	}
+
+    free (manline);
+    return 0;
+}
